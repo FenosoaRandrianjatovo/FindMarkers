1f464e6904446e8e20bf5c1f38b98f2e3c96ac2b
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2025-02-24 14:53:19 -0500

    Create UMAP_analysis_Exploration.R

---
+++ Merge/UMAP_analysis_Exploration.R
@@ -0,0 +1,34 @@
+library(Seurat)
+library(future)
+library(ggplot2)
+library(arrow)
+library(glmGamPoi)
+library(patchwork)
+library(RColorBrewer)
+library(patchwork)
+plan("multisession", workers = 25)
+
+print("Set the working directory")
+setwd("/home/fenosoa/scratch/Maya_Project/FindMarkers/images")
+
+path <- normalizePath("/home/fenosoa/scratch/Maya_Project/merge_data/UMAP_xenium_merge.obj.RData")
+
+temp_env <- new.env()
+print("xenium.obj :   Loading begin")
+load(path, envir = temp_env)
+
+xenium.obj <- temp_env$UMAP_xenium_merge.obj
+
+groups <- levels(xenium.obj)
+
+print("Nomber of Cluster")
+print(length(groups))
+
+n <- length(groups) 
+
+print("==================================================================================")
+plot4 <- DimPlot(xenium.obj)
+ggsave("UMAP_merge_Plot.png", plot = plot4, width = 20, height = 15, dpi = 300)
+
+plot1 <- DimPlot(xenium.obj, fov="fov.1")
+ggsave("UMAP_merge_Plot.png", plot = plot4, width = 20, height = 15, dpi = 300)
